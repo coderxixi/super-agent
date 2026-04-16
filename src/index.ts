6fe0b1234566169a70954c66d52821cf34b5ec5f
coderxixi <976344695@qq.com>
2026-04-16 13:51:40 +0800

    Update index.ts
    把 generateText 换成 streamText

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@
  * @Author: coderxixi 976344695@qq.com
  * @Date: 2026-04-16 09:57:47
  * @LastEditors: coderxixi 976344695@qq.com
- * @LastEditTime: 2026-04-16 13:40:42
+ * @LastEditTime: 2026-04-16 13:50:40
  * @FilePath: /super-agent/src/index.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 import 'dotenv/config';
-import { generateText } from 'ai';
+import { streamText } from 'ai';
 import { createOpenAI } from '@ai-sdk/openai';
 import { createMockModel } from './mock-model';
 const qwen = createOpenAI({
@@ -20,12 +20,16 @@ const model = process.env.DASHSCOPE_API_KEY
   : createMockModel();
 
 async function main() {
-  const { text } = await generateText({
+  const result =  streamText({
     model,
     prompt: '用一句话介绍你自己',
   });
 
-  console.log(text);
+  for await (const chunk of result.textStream) {
+    process.stdout.write(chunk);
+  }
+
+  console.log(); // 换行
 }
 
 main();
